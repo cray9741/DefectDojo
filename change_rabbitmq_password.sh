7cb2a942dacb25606a2608e8a16e7aa6fd3e17fa
cray9741 <168498517+cray9741@users.noreply.github.com>
2024-06-26 22:15:07 -0400

    Update change_rabbitmq_password.sh

--- change_rabbitmq_password.sh
+++ change_rabbitmq_password.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Step 1: Get the rabbitmq password
-PASSWORD=$(kubectl get secret defectdojo-rabbitmq-specific -o jsonpath="{.data.rabbitmq-password}" | base64 --decode)
+PASSWORD=$(kubectl get secret defectdojo-rabbitmq-specific -n defectdojo -o jsonpath="{.data.rabbitmq-password}" | base64 --decode)
 
 # Step 2: Execute the command in the defectdojo-rabbitmq-0 pod
-kubectl exec -it defectdojo-rabbitmq-0 -n default -- /bin/bash -c "
+kubectl exec -it defectdojo-rabbitmq-0 -n defectdojo -- /bin/bash -c "
     # Step 3: Change the rabbitmq password
     rabbitmqctl change_password user $PASSWORD
 "
